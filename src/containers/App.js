550eb68775586e9fe6b8d7184bcb54cabef8741a
Jason Andrada <jpa323@nyu.edu>
2019-01-25 23:08:34 -0500

    move App.js file to container folder

---
+++ src/containers/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { makeSearch, buildQuery } from '../actions'
+import PropTypes from 'prop-types';
+import Search from '../components/Search';
+
+const App = ({ makeSearch, buildQuery }) => (
+  <div className="App">
+    <Search makeSearch={makeSearch} buildQuery={buildQuery} />
+  </div>
+);
+
+const mapStateToProps = (state) => ({
+  searchQuery: state.searchQuery
+})
+
+App.propTypes = {
+  makeSearch: PropTypes.func,
+  buildQuery: PropTypes.func
+}
+
+export default connect(mapStateToProps, { makeSearch, buildQuery })(App);
