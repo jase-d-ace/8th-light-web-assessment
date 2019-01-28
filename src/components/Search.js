85631a648fb40fe5400964c0ec961f8f7679f08d
Jason Andrada <jpa323@nyu.edu>
2019-01-28 15:55:48 -0500

    hook up search to redux store to render book data from API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import services from '../services/services';
+import List from './List';
 
-const Search = ({ makeSearch, buildQuery }) => (
+/*TODO:
+ * Build logic to actually show the stuff onscreen
+ * Add a ternary that renders either an error component or the results
+ * Decide whether I want to keep the search bar onscreen at all times or render that conditionally as well.
+*/
+
+const Search = ({ makeSearch, buildQuery, promiseResolved, err, queryResult }) => (
   <div className="search">
     <input type="text" onChange={(e) => services.inputChange(e.target.value, buildQuery)} />
     <button onClick={(e) => services.submit(e, makeSearch)}>Click Me</button>
+    {promiseResolved ? <List queryResult={queryResult} /> : err ? <h1>Something went wrong!</h1> : <h1>Please Make a Search</h1>}
   </div>
 );
 
-export default Search;
+const mapStateToProps = (state) => ({
+  queryResult: state.search.queryResult,
+  promiseResolved: state.search.promiseResolved,
+  err: state.search.err
+})
+
+export default connect(mapStateToProps)(Search)
