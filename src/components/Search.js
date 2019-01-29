b42d584842b5b252b6d91d4eb7159f6657131930
Jason Andrada <jpa323@nyu.edu>
2019-01-28 21:03:20 -0500

    make search into connected component, add proptypes, and add render logic

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import services from '../services/services';
+import PropTypes from 'prop-types';
 import List from './List';
 
 /*TODO:
- * Build logic to actually show the stuff onscreen
- * Add a ternary that renders either an error component or the results
  * Decide whether I want to keep the search bar onscreen at all times or render that conditionally as well.
 */
 
@@ -13,7 +12,7 @@ const Search = ({ makeSearch, buildQuery, promiseResolved, err, queryResult }) =
   <div className="search">
     <input type="text" onChange={(e) => services.inputChange(e.target.value, buildQuery)} />
     <button onClick={(e) => services.submit(e, makeSearch)}>Click Me</button>
-    {promiseResolved ? <List queryResult={queryResult} /> : err ? <h1>Something went wrong!</h1> : <h1>Please Make a Search</h1>}
+    {promiseResolved && !err ? <List queryResult={queryResult} /> : err ? <h1>Something went wrong!</h1> : <h1>Please Make a Search</h1>}
   </div>
 );
 
@@ -22,5 +21,20 @@ const mapStateToProps = (state) => ({
   promiseResolved: state.search.promiseResolved,
   err: state.search.err
 })
+
+Search.propTypes = {
+  makeSearch: PropTypes.func.isRequired,
+  buildQuery: PropTypes.func.isRequired,
+  promiseResolved: PropTypes.bool,
+  err: PropTypes.object,
+  queryResult: PropTypes.arrayOf(PropTypes.shape({
+    etag: PropTypes.string.isRequired,
+    authors: PropTypes.arrayOf(PropTypes.string),
+    image: PropTypes.string,
+    publisher: PropTypes.string,
+    title: PropTypes.string,
+    info: PropTypes.string
+  }))
+}
 
 export default connect(mapStateToProps)(Search)
