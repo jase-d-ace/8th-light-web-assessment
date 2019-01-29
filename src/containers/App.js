9c4de82d15ad9863b5bcf2d552ceb6e2ba4fb087
Jason Andrada <jpa323@nyu.edu>
2019-01-29 16:12:49 -0500

    reconfigure App to be a render-only component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { makeSearch, buildQuery } from '../actions'
-import PropTypes from 'prop-types';
 import Search from '../components/Search';
 
-const App = ({ makeSearch, buildQuery }) => (
+const App = () => (
   <div className="App">
-    <Search 
-      makeSearch={makeSearch}
-      buildQuery={buildQuery}
-    />
+    <Search />
   </div>
 );
 
-App.propTypes = {
-  makeSearch: PropTypes.func,
-  buildQuery: PropTypes.func
-}
-
-export default connect(null, { makeSearch, buildQuery })(App);
+export default App
