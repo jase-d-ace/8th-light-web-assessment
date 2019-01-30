47ff78536269c5ba6730ac1be57f57b0157d822f
Jason Andrada <jpa323@nyu.edu>
2019-01-30 15:16:24 -0500

    add another test for reducer

--- src/reducers/search.spec.js
+++ src/reducers/search.spec.js
@@ -16,5 +16,15 @@ describe('reducers', () => {
     it('should change searchQuery based a string', () => {
       expect(search(initialState, {type: 'SEARCH_QUERY', query: 'infinite jest'})).toEqual({...initialState, searchQuery: "infinite jest"})
     })
+    it('should hold onto the last search in state', () => {
+      const state = {
+        searchQuery: 'example',
+        queryResult: null,
+        promiseResolved: true,
+        err: null,
+        previousQuery: null
+      };
+      expect(search(state, { type: 'QUERY_RESOLVED', query: [{foo: 'bar'}], prev: 'example'})).toEqual({...state, searchQuery: null, queryResult: [{foo: 'bar'}], previousQuery: 'example'})
+    })
   })
 })
