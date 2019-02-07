01140630b36fc64661e17ec49bc8ddbe797619b1
Jason Andrada <jpa323@nyu.edu>
2019-02-07 14:05:50 -0500

    reformat text for search results

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -8,7 +8,6 @@ const ListItem = ({title, id, authors, thumbnail, infoLink, publisher}) => {
    * Sometimes, the API doesn't return authors at all.
    * For the most part, the data that comes back is pretty consistent, being arrays of length 1.
    * For the exceptions, this check returns a string of all the authors separated by commas. 
-   * Not exactly the cleanest solution, and leads to some awkward formatting when viewed, but it gets the job done.
    * The check first looks to see if authors are defined at all (in some cases, authors comes back undefined)
    * Then it checks to see if there is more than one author listed.
    * If there is, map over and separate by comma. If not, just return the first (and only) author.
@@ -18,14 +17,15 @@ const ListItem = ({title, id, authors, thumbnail, infoLink, publisher}) => {
     //destructure the reversed array, putting the first entries at the end since es6 doesn't allow destructuring in the beginning
     const [beginning, ...end] = arr.reverse();
     //join the end of the reversed array (the beginning of the original) with commas, and drop ', and ' before the last
-    return end.join(', ') + ', and ' + beginning
+    return <span>{`${end.join(', ')}, and ${beginning}`}</span>
   }
   const authorCheck = authors ? authors.length > 1 ? (separateAuthors((authors))) : (<span>{authors[0]}</span>) : (<span>Author Unavailable</span>)
 
   return(
     <li className="book-item">
     {thumbnail ? (<img src={thumbnail}alt="book" />) : <div className="placeholder"><span className="placeholder-text">No Image Available</span></div>}
-      <p>{title}, by {authorCheck}</p> 
+      <h4>{title}</h4>
+      <p> by {authorCheck}</p>
       <p>Publisher: {publisher ? publisher : "Publisher Unavailable"}</p>
       <p>More Info: <a target="_blank" rel="noopener noreferrer" href={infoLink}>Click Here</a></p>
     </li>
