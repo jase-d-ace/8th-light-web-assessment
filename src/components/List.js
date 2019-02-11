c1b493efc052f422c8d2c7be1782a4f9decfacaa
Jason Andrada <jpa323@nyu.edu>
2019-02-11 16:08:53 -0500

    add presentational logic and styles to paginated component

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ListItem from './ListItem';
+import { connect } from 'react-redux';
+import { pageUp, pageDown } from '../actions';
 
-const List = ({ queryResult }) => {
+const List = ({ queryResult, offset, pageUp, pageDown }) => {
   const nodes = queryResult.map(book => <ListItem key={book.id} {...book} />)
   return (
-    <ol className="book-list">
-      {nodes}
-    </ol>
+    <div className="list-container">
+      <ol className="book-list">
+        {nodes}
+      </ol>
+      <div className="button-container">
+        <button disabled={offset===0} onClick={pageDown}>Previous Page</button> 
+        <button disabled={queryResult.length<40} onClick={pageUp}>Next Page</button>
+      </div>
+    </div>
   );
 };
-export default List;
+
+const mapStateToProps = state => ({
+  offset: state.search.offset
+})
+
+export default connect(mapStateToProps, { pageUp, pageDown })(List)
