6401af82d52fb9b12383f9d109bf9e9efe67c16a
Jason Andrada <jpa323@nyu.edu>
2019-02-10 23:45:24 -0500

    refactor error component to handle different errors

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-/*
- * Obviously this component is kind of a joke.
- * It serves a very real purpose of handling an error, but does so in a way that isn't particularly helpful to the user.
- * If this was meant to be a serious component, there might be some logic here that maybe suggests "close" matches?
- * As it stands, this component is here only to stop the entire app from crashing in case the user messes something up
-*/
+const errorCheck = err => {
+  switch(err.message) {
+    case "Your search returned no results":
+      return <h2>Your search returned no results</h2>
+    default:
+      return (
+      <div className="error">
+        <p>To make everything better, here's a random stock photo.</p>
+        <img src="https://picsum.photos/200" alt="error" />
+      </div>
+    )
+  }
+}
 
-const Error = ({ err }) => (
-  <div className="error">
-    <h1>Something went wrong with your search!</h1>
-    <p>To make everything better, here's a random stock photo.</p>
-    <img src="https://picsum.photos/200" alt="error" />
-    <h2>Maybe try your search again or try a different search?</h2>
-  </div>
-);
+const Error = ({ err }) => errorCheck(err)
 
 Error.propTypes = {
   err: PropTypes.shape({
